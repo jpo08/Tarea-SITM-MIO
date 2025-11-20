1e88ba187e23514442b662ca99e879710b874d05
jpo08 <juanpabloordonezjimenez@gmail.com>
2025-11-20 10:01:03 -0500

    grafico

--- Main.java
+++ Main.java
@@ -9,6 +9,32 @@ public class Main {
 
         Map<String, List<Arc>> arcs = GraphBuilder.buildArcs(lineStops);
 
+                for (String key : arcs.keySet()) {
+            String[] parts = key.split("-");
+            int lineId = Integer.parseInt(parts[0]);
+            int orientation = Integer.parseInt(parts[1]);
+            int variant = Integer.parseInt(parts[2]);
+
+            Line ln = lines.get(lineId);
+
+            System.out.println("\n==========================================");
+            System.out.println("Ruta: " + ln.shortName + 
+                               " | ID: " + lineId +
+                               " | Orientación: " + orientation +
+                               " | Variante: " + variant);
+            System.out.println("------------------------------------------");
+
+            for (Arc a : arcs.get(key)) {
+                Stop from = stops.get(a.from);
+                Stop to = stops.get(a.to);
+
+                System.out.println(
+                    a.from + " (" + from.shortName + ") → " +
+                    a.to + " (" + to.shortName + ")"
+                );
+            }
+        }
+
         System.out.println("Generando mapa global del SITM-MIO...");
 
         SystemMapRenderer.drawFullSystem(
